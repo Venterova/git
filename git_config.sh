bcbcad1601a34da864e356ca56614613ed1ad970
Honza Běhunčík <honzabehuncik@gmail.com>
2024-09-27 08:32:05 +0200

    add script to repo

---
+++ git_config.sh
@@ -0,0 +1,6 @@
+#!bin/gash
+echo "welcome to Git config"
+git config -- global user.email "vanessaventerova@gmail.com"
+git config --global user.name "vanessa venterova"
+git config --list
+echo "done successfully"
